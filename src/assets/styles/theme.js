e037d2c7bef7392d279d7861ca4f9680627e4eb3
Mateusz Iwaniszczuk <mrvanosh@gmail.com>
2020-03-16 21:51:07 +0100

    Shorthands for theming

---
+++ src/assets/styles/theme.js
@@ -0,0 +1,8 @@
+export default {
+  mq: {
+    m: `@media (min-width: 768px)`,
+    l: `@media (min-width: 1024px)`,
+    xl: `@media (min-width: 1280px)`,
+    xxl: `@media(min-width: 1800px)`,
+  },
+}
